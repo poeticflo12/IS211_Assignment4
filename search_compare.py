678a5c5ef2a8d6009e4e2d585fab8c0239241847
poeticflo12 <gelaneymatthew@gmail.com>
2020-03-10 15:54:52 -0400

    testing

--- search_compare.py
+++ search_compare.py
@@ -1,1 +1,20 @@
-test
+#!python3
+import time
+import random
+
+
+# part 1
+def sequential_search(a_list, item):
+    start = time.process_time()
+    pos = 0
+    found = False
+    while pos < len(a_list) and not found:
+        if a_list[pos] == item:
+            found = True
+        else:
+            pos = pos + 1
+
+    end = time.process_time()
+    timeTaken = end - start
+
+    return [found, timeTaken]
